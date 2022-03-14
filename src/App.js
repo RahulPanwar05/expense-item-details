f0b3aebd143713649c6227ffd51ad66ca3f26186
Rahul Panwar <rahul.panwar900@gmail.com>
2022-03-14 12:25:18 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+
+import ExpenseItem from './components/ExpenseItem.js';
 import './App.css';
+function App(){
+  let  expennse=[
+      { 
+          id:"e1",
+        title:'School fee',
+        amount:250,
+        date: new Date(2022, 3, 14)
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      },
+      { 
+        id:"e2",
+      title:'Books',
+      amount:230,
+      date: new Date(2022, 3, 15)
+
+    },
+      { 
+        id:"e3",
+      title:'House Rent',
+      amount:700,
+      date: new Date(2022, 3, 15)
+
+    },
+      { 
+        id:"e4",
+      title:'Food',
+      amount:540,
+      date: new Date(2022, 3, 16)
+
+    }
+  ]
+    return ( 
+        <div>
+          <div className='heading'><h2>Track Your Monthly Expense Item Detial</h2></div>
+            <ExpenseItem 
+            date={expennse[0].date} 
+            title={expennse[0].title} 
+            amount={expennse[0].amount}>
+            </ExpenseItem>
+            <ExpenseItem 
+            date={expennse[1].date} 
+            title={expennse[1].title} 
+            amount={expennse[1].amount}>
+            </ExpenseItem>
+            <ExpenseItem 
+            date={expennse[2].date} 
+            title={expennse[2].title} 
+            amount={expennse[2].amount}>
+            </ExpenseItem>
+            <ExpenseItem 
+            date={expennse[3].date} 
+            title={expennse[3].title} 
+            amount={expennse[3].amount}>
+            </ExpenseItem>
+           
+           
+        </div>
+
+    )
 }
-
 export default App;
